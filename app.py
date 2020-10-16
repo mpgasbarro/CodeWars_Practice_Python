68c085d42e44ab693129776c3b43a79371eb9393
Gaspy <mpgasbarro@gmail.com>
2020-10-16 16:07:31 -0400

    first commit for python codewars

---
+++ app.py
@@ -0,0 +1,65 @@
+# time = 23
+
+# if time < 9:
+#     print("Morning is wonderful. its only drawback is that it comes at such an incovenient time of day")
+# elif time <= 16 and time > 9:
+#     print("Working hard or hardly working")
+# elif time < 20 and time > 16:
+#     print("How did it get so late so soon")
+# elif time < 22 and time > 20:
+#     print("A daty without sunshine is like, you know, night")
+# else:
+#     print("Burning the midnight oil!")
+# _______________________________________
+
+# angry = True
+# bored = True
+# hungry = True
+# tired = False
+
+# if hungry and bored and angry:
+#     print("Ya boy is going to eat a Triceratops")
+# elif tired and hungry:
+#     print("ya boy is going to eat the iguanadon")
+# elif hungry and bored:
+#     print("ya boy is going to eat the Stegasaurus")
+# elif tired:
+#     print("Ya boy is taking a nap")
+# elif angry and bored:
+#     print("Ya boy is fighting a velociraptor with Chris Pratt")
+# elif angry and bored:
+#     print("ROARRRRRRRR")
+# else:
+#     print("ya boy gives a toothy smile, whatever that means")
+# __________________________________________________________________
+# disney_characters = ['simba', 'ariel', 'pumba', 'flounder', 'nala', 'ursula', 'scar', 'flotsam', 'timon']
+
+# for i in range(0, len(disney_characters[0: : ])):
+#     if "i" in "disney_characters[i]:
+#         print("I bet you're Impressively Intelligent!")
+# -_________________________________________________________________________________________
+# def get_average(marks):
+#     summ = 0
+#     length = len(marks)
+#     for i in marks:
+#        summ += i
+#     total = summ // length
+#     return(total)
+
+# num = [1,23,4,5]
+
+# print(get_average(num))
+# _________________________________________________________________________
+# return masked string
+# def maskify(cc):
+#     length = len(cc)
+#     whereToCut = length - 4
+#     index = cc.index(whereToCut)
+#     print(index)
+#     newArr = cc.split(whereToCut)
+#     print(newArr)
+
+
+# stri = "1234567"
+
+# maskify(stri)
