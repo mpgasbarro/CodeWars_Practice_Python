119287d0e0cfe9891c8cb870643451c2d2f0451c
Gaspy <mpgasbarro@gmail.com>
2020-10-18 13:00:55 -0400

    second solution for problem 10/17/20

--- app.py
+++ app.py
@@ -231,3 +231,34 @@ heroes = planeteers + rangers
 #     return new_new
 
 # add_length("hello there")
+
+
+
+
+# ____________________________________________________
+# CODEWARS PRACTIE PROBLEM 10/17/20
+# Given two integer arrays a, b, both of length >= 1, create a program that returns true if the sum of the squares of each element in a is strictly greater than the sum of the cubes of each element in b.
+# E.g.
+# array_madness([4, 5, 6], [1, 2, 3]) => True #because 4 ** 2 + 5 ** 2 + 6 ** 2 > 1 ** 3 + 2 ** 3 + 3 ** 3
+# Get your timer out. Are you ready? Ready, get set, GO!!!
+
+
+# SOLUTION
+# def array_madness(a,b):
+#     sumA = 0
+#     sumB = 0
+#     for i in a:
+#         sumA = sumA + (i ** 2)
+        
+#     for b in b:
+#         sumB = sumB + (b ** 2)
+    
+#     if sumA > sumB:
+#         return True
+#     else:
+#          return False
+     
+
+
+
+# array_madness([0, 4, 123], [4, 5, 6])
