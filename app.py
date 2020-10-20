f6cfeb656a5b5433267b91894f0bc69a6dcf5844
Gaspy <mpgasbarro@gmail.com>
2020-10-20 07:22:49 -0400

    solved first problem - 10/20/20

--- app.py
+++ app.py
@@ -279,3 +279,28 @@ heroes = planeteers + rangers
 
 
 # positive_sum([-4,9,-7,-1,-2, -3])
+# /-_____________________________________________________
+
+
+# CODEWARS PROBLEM - 10/20/20
+
+# Create a function with two arguments that will return an array of the first (n) multiples of (x).
+# Assume both the given number and the number of times to count will be positive numbers greater than 0.
+# Return the results as an array (or list in Python, Haskell or Elixir).
+# Examples:
+# count_by(1,10) #should return [1,2,3,4,5,6,7,8,9,10]
+# count_by(2,5) #should return [2,4,6,8,10]
+
+
+# *****SOLUTION*******
+# def count_by(x, n):
+#     sum = x * n
+#     arr = []
+#     total = range(x, sum + 1, x)
+
+#     for i in total:
+#         arr.append(i)
+    
+#     return(arr)
+
+# count_by(2, 5)
