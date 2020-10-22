1a27118ff47ce44bb10537f6d294c65c6fc9a59a
Gaspy <mpgasbarro@gmail.com>
2020-10-22 12:27:04 -0400

    Python Codewar problem - 10/22/20 - I was not able to solve this problem on my own. Have been reviewing the solution to get a better understanding

--- app.py
+++ app.py
@@ -386,21 +386,54 @@ heroes = planeteers + rangers
 
 
 # ***** SOLUTION *****
-def rps(p1, p2):
-    if p1 == "scissors" and p2 == "paper":
-        return "Player 1 won!"
-    elif p1 == "paper" and p2 == "scissors":
-        return "Player 2 won!"
-    elif p1 == "rock" and p2 == "paper":
-        return "Player 2 won!"
-    elif p1 == "paper" and p2 == "rock":
-        return "Player 1 won!"
-    elif p1 == "rock" and p2 == "scissors":
-        return "Player 1 won!"
-    elif p1 == "scissors" and p2 == "rock":
-        return "PLayer 2 won!"
-    elif p1 == p2:
-        return "Draw!"
+# def rps(p1, p2):
+#     if p1 == "scissors" and p2 == "paper":
+#         return "Player 1 won!"
+#     elif p1 == "paper" and p2 == "scissors":
+#         return "Player 2 won!"
+#     elif p1 == "rock" and p2 == "paper":
+#         return "Player 2 won!"
+#     elif p1 == "paper" and p2 == "rock":
+#         return "Player 1 won!"
+#     elif p1 == "rock" and p2 == "scissors":
+#         return "Player 1 won!"
+#     elif p1 == "scissors" and p2 == "rock":
+#         return "PLayer 2 won!"
+#     elif p1 == p2:
+#         return "Draw!"
     
 
-rps("scissors", "paper")
+# rps("scissors", "paper")
+
+# ____________________________________________________________
+
+
+# CODEWARS PROBLEM 10/22/20
+
+
+# Challenge:
+
+# Given a two-dimensional array, return a new array which carries over only those arrays from the original, which were not empty and whose items are all of the same type (i.e. homogenous). For simplicity, the arrays inside the array will only contain characters and integers.
+
+# Example:
+
+# Given [[1, 5, 4], ['a', 3, 5], ['b'], [], ['1', 2, 3]], your function should return [[1, 5, 4], ['b']].
+
+# Addendum:
+
+# Please keep in mind that for this kata, we assume that empty arrays are not homogenous.
+
+# The resultant arrays should be in the order they were originally in and should not have its values changed.
+
+# No implicit type casting is allowed. A subarray [1, '2'] would be considered illegal and should be filtered out.
+
+
+# ***** SOLUTION *****I WAS NOT ABLE TO SOLVE THIS****
+
+def filter_homogenous(arrays):
+    return[a for a in arrays if len(set(map(type,a)))==1]
+
+
+
+
+filter_homogenous([[1, 5, 4], ['a', 3, 5], ['b'], [], ['1', 2, 3]])
