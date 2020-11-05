72365eced8da7e3ff5d9a7b8a6ed78e7b5a0789a
Gaspy <mpgasbarro@gmail.com>
2020-11-05 12:19:04 -0500

    solved Kata-6 for python 11/05/20

--- app.py
+++ app.py
@@ -450,21 +450,45 @@ heroes = planeteers + rangers
 
 
 # SOLUTION  - Still working on getting last dash out of last index
-def accum(s):
-    new_Arr = []
-    val=""
-    for char in s:
-        new_Arr.append(char)
+# def accum(s):
+#     new_Arr = []
+#     val=""
+#     for char in s:
+#         new_Arr.append(char)
         
-    for i in new_Arr:
-        val = new_Arr.index(i) + 1
-        last_Ind = new_Arr[len(new_Arr) -1]
-        ind = len(new_Arr)
-        new_Arr.pop(new_Arr.index(i))
-        new_Arr.insert(val - 1, (i * val))
-    result = "-".join(str(i).capitalize() for i in new_Arr)
+#     for i in new_Arr:
+#         val = new_Arr.index(i) + 1
+#         last_Ind = new_Arr[len(new_Arr) -1]
+#         ind = len(new_Arr)
+#         new_Arr.pop(new_Arr.index(i))
+#         new_Arr.insert(val - 1, (i * val))
+#     result = "-".join(str(i).capitalize() for i in new_Arr)
    
     
-    print(result)
+#     print(result)
 
-accum("abcd")
+# accum("abcd")
+
+
+# _______________________________________________________________________
+
+# CODEWAR PRACTICE  - 11/05/20
+# If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+# Finish the solution so that it returns the sum of all the multiples of 3 or 5 below the number passed in.
+
+# Note: If the number is a multiple of both 3 and 5, only count it once. Also, if a number is negative, return 0(for languages that do have them)
+
+def solution(number):
+    sum = 0
+    for i in range(0, number):
+        if i % 3 == 0 and i % 5 != 0:
+            sum += i
+        if i % 5 == 0 and i % 3 != 0:
+            sum += i
+        if i % 5 == 0 and i % 3 == 0:
+            sum += i
+
+    print(sum)
+
+solution(16)
